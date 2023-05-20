7ccc63ced72ee6183a6febf8190e64a14b4dfe59
Dirk van Zon <dirk@moeilijkedingen.nl>
2023-05-20 11:10:54 +0200

    first version of playlistthumbnail component

---
+++ src/components/Playlist.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-card>
+        <v-sheet class="d-flex">
+            <v-sheet class="ma-2 pa-0 me-auto">
+                <v-img
+                    aspect-ratio="1:1"
+                    height="150"
+                    width="150"
+                    :src="playlist.image"
+                ></v-img>
+            </v-sheet>
+            <v-sheet class="ma-2 pa-2 ">
+                <v-card-title v-text="playlist.name" class="text-h5"></v-card-title>
+                <v-card-text v-text="playlist.creator"></v-card-text>
+            </v-sheet>
+            <v-sheet class="ma-2 pa-2">
+                <v-btn-group>
+                    <v-btn icon @click="click()" small>
+                        <v-icon>
+                            mdi-page-next
+                        </v-icon>
+                    </v-btn>
+                    <v-btn icon :href="playlist.url" target="_blank" small>
+                        <v-icon>
+                            mdi-open-in-new 
+                        </v-icon>
+                    </v-btn>
+                </v-btn-group>
+            </v-sheet>
+        </v-sheet>
+    </v-card>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PlaylistThumbnail',
+  props: ["playlist"],
+  methods: {
+    click() {
+        alert(this.playlist.id)
+    }
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-      <a>{{ userPlaylists}}</a>
+    <v-col 
+      md="4"
+      offset-md="4"
+      v-for="item in userPlaylists" 
+      :key="item.uri" 
+      :cols="item.flex">
+        <PlaylistThumbnail v-bind:playlist="item"></PlaylistThumbnail>
+    </v-col>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import PlaylistThumbnail  from "./../components/Playlist.vue";
 
 export default defineComponent({
   name: 'HomeView',
+  components: {
+    PlaylistThumbnail,
+  },
   async mounted() {
     await this.$store.dispatch("getUsersPlaylists");
   },
