b83c6a2058eddb53b629501201ceb8fbc483a556
Dirk van Zon <dirk@moeilijkedingen.nl>
2023-06-20 11:52:31 +0200

    track card styling

--- src/components/CassetteSide.vue
+++ src/components/CassetteSide.vue
@@ -15,52 +15,28 @@
             </v-btn>
         </v-col>
       </v-row>
-        <draggable 
-        v-model="tracks" 
-        :group="`tracks`"
-        item-key="id">
-            <template #item="{element}">
-                <v-card tile class="ma-1" :disabled="element.hidden">
-                  <v-row style="margin: 1px">
-                    <v-col md="2">
-                          <v-img :src="element.image"></v-img>
-                    </v-col>
-                    <v-col md="1">
-                      <v-row>
-                        <v-card-title v-text="element.name" style="margin: 0"></v-card-title>
-                      </v-row>
-                      <v-row
-                        v-for="artist in element.artists"
-                        :key="artist"
-                        style="padding: 2px">
-                        <v-card-subtitle v-text="`${artist}; `"></v-card-subtitle>
-                      </v-row>
-                    </v-col>
-                    <v-col md="2" offset-md="7">
-                        <v-btn-group>
-                          <v-btn icon x-small @click="SetHidden(element.id)">
-                              <v-icon>
-                                  mdi-eye-off
-                              </v-icon>
-                          </v-btn>
-                      </v-btn-group>
-                    </v-col>
-                  </v-row>
-                </v-card>
-            </template>
-        </draggable>
+          <draggable 
+            v-model="tracks" 
+            :group="`tracks`"
+            item-key="id">
+              <template #item="{element}">
+                <Track v-bind:track="element"></Track>
+              </template>
+          </draggable>
     </v-card>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue'
 import draggable from 'vuedraggable'
+import Track from '@/components/Track.vue'
 
 export default defineComponent({
   name: 'CassetteSide',
   props: ["sideIndex"],
   components: {
     draggable,
+    Track,
   },
   computed: {
     tracks: {
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,7 +1,24 @@
-<template>
-    <div>
-        {{ track }}
-    </div>
+<template>  
+  <div>
+    <v-card flat>
+      <v-row no-gutters justify="start">
+        <v-col xs="12" sm="3" md="6" lg="3" cols="2">
+            <v-img 
+              :src="track.image" 
+              :width="track.duration / 2000"
+              min-width="50"
+              max-width="120"
+              class="ma-1"
+            ></v-img>
+        </v-col>
+        <v-col xs="12" sm="7" md="6" lg="7">
+          <v-card-title v-text="track.name"></v-card-title>
+          <v-card-subtitle v-text="track.artists.join('; ')"></v-card-subtitle>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-divider></v-divider>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -12,3 +29,16 @@ export default defineComponent({
   props: ["track"],
 })
 </script>
+
+  <!-- <v-card tile :disabled="track.hidden" flat  :height="50">
+    <v-row style="margin: 1px" :height="50">
+      <v-col>
+        <v-img 
+          :src="track.image" 
+          aspect-ratio="1"
+          height="auto"
+        ></v-img>
+      </v-col>
+    </v-row>
+  </v-card>
+  <v-divider></v-divider> -->
